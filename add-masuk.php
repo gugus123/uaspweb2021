8a55757101e6a6598bd54b228158bdcdf012fa08
gugus123 <guguslalu298@gmail.com>
2021-07-25 15:46:20 +0800

    first commit

---
+++ add-masuk.php
@@ -0,0 +1,45 @@
+<h3> tambah barang masuk </h3>
+
+<form action="" method="post">
+    <table>
+        <tr>
+            <td width="130">id barang</td>
+            <td><input type="text" name="idbarang"></td>
+        </tr>
+        <tr>
+            <td>nama barang</td>
+            <td><input type="text" name="namabarang"></td>
+        </tr>
+        <tr>
+            <td>deskripsi</td>
+            <td><input type="text" name="deskripsi"></td>
+        </tr>
+        <tr>
+            <td>stok</td>
+            <td><input type="text" name="stok"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="tambah" name="proses"></td>
+        </tr>
+    </table>
+</form>
+
+
+
+
+
+
+
+
+<?php
+include "function.php";
+if (isset($_POST['proses'])){
+    mysqli_query($conn,"insert into stock set
+    idbarang ='$_POST[idbarang]',
+    namabarang ='$_POST[namabarang]',
+    deskripsi ='$_POST[deskripsi]',
+    stok ='$_POST[stok]'");
+    echo "data stock telah tersimpan"; 
+}
+    ?>
